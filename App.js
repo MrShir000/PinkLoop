2bbd90ede6b6d5d1ed3f71b0cffcae99d547e8c8
MorganaRodrigues <morgana.r1@aluno.ifsc.edu.br>
2020-03-11 16:25:38 -0300

    Ex Morgana code

--- App.js
+++ App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Pink Loop 2 </Text>
+    return (
+    <View style={styles.container} >
+        <Text> Pink Loop 3 </Text>
+        <Text> Morgana </Text > 
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
